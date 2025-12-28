a5fafadcbdebf89bd0c6b5d7291eb5c518be098f
ppkiskpr <newyear772023@gmail.com>
2025-12-28 12:19:52 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
 // config.js
-// Jika nak guna CSV dari luar (Google Sheet/AppSheet export), letak URL di bawah.
-// Kalau kosong, sistem akan guna ./data/kehadiran.csv
-
 window.PORTAL_CONFIG = {
   SCHOOL_NAME: "PPKI SK Paya Resak",
   CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTKhex5gcCHKoiLqRZpgVRMRfrGGvPtbgc3jo_Q-3FPL5OpcoWvyAnec0dTgykrpkYuS34iNJnEAkDU/pub?gid=880667725&single=true&output=csv",
